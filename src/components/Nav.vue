0c20f1a37e189e44c3b8511d9506c3634d6a2237
Naka4442 <nak4442@gmail.com>
2022-07-15 18:24:32 +0300

    Доделанная навигация

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -5,6 +5,12 @@
             <li><img src="../assets/icons/plus.png" alt=""></li>
             <li><img src="../assets/icons/menu.png" alt=""></li>
         </ul>
+        <div class="show">
+            <img src="../assets/icons/open-menu.png" alt="">
+        </div>
+        <div class="help">
+            <img src="../assets/icons/help.png" alt="">
+        </div>
     </nav>
 </template>
 <script>
@@ -17,6 +23,11 @@ nav{
     width: 80px;
     height: 100vh;
     background-color: #223a45;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
     ul{
         margin: 0;
         list-style-type: none;
@@ -32,6 +43,34 @@ nav{
                 width: 100%;
                 height: 100%;
             }
+        }
+    }
+    .show{
+        display: flex;
+        align-self: flex-end;
+        justify-content: center;
+        align-items: center;
+        width: 40px;
+        height: 80px;
+        border-bottom-left-radius: 40px;
+        border-top-left-radius: 40px;
+        background-color: rgba(255, 255, 255, 0.2);
+        img{
+            transform: rotate(180deg);
+            transition: .5s;
+            &:hover{
+                transition: .5s;
+                transform: rotate(0deg);
+            }
+        }
+    }
+    .help{
+        width: 24px;
+        height: 24px;
+        padding-bottom: 24px;
+        img{
+            width: 100%;
+            height: 100%;
         }
     }
 }
